7149bba809feedf4ab7b56c5ebe9662d75e74e4a
kaorish <nono00520822@gmail.com>
2025-06-20 18:39:15 +0800

    first commit

---
+++ src/main/java/com/sunhao/courseschedulebackend/CourseScheduleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunhao.courseschedulebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseScheduleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseScheduleBackendApplication.class, args);
+	}
+
+}
